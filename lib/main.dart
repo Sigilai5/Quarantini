1238cd6f0ae6e16646c7497b17cd4fd498a4c1a5
sigilai <briansigilai@gmail.com>
2020-11-10 22:34:06 +0300

    removed animated splash screen package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instaflutter/pages/home.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized(); //The reason behind this is I am waiting for some data or running an async function inside main().
